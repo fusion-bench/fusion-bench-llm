89e35428408f74ad6b203ad3b52cbcda1a753f53
tanganke <tang.anke@foxmail.com>
2025-07-23 15:53:14 +0800

    feat: add new configuration files and CLI for fusion-bench-llm, including model pools and logging setups

--- scripts/merge/simple_average.sh
+++ scripts/merge/simple_average.sh
@@ -1,8 +1,14 @@
 script_dir=$(dirname "$(realpath "$0")")
 project_root=$(realpath "${script_dir}/../..")
 
-fusion_bench --config-dir ${project_root}/config \
+rich-run fusion_bench --config-dir ${project_root}/config \
     method=linear/simple_average_for_llama \
     modelpool=Qwen2.5-1.5B_math_and_coder \
     merged_model_save_path=${project_root}/outputs/qwen2.5-1.5B_math_and_coder/simple_average \
+    +merged_model_save_kwargs.save_tokenizer=true
+
+rich-run fusion_bench --config-dir ${project_root}/config \
+    method=linear/simple_average_for_llama \
+    modelpool=Qwen2.5-7B_math_and_coder \
+    merged_model_save_path=${project_root}/outputs/qwen2.5-7B_math_and_coder/simple_average \
     +merged_model_save_kwargs.save_tokenizer=true
